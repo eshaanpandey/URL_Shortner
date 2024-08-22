c528f7f613a488214a77686d3e3b1cfe28ba6a8a
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2024-08-22 12:10:08 +0530

    url shortner

--- controllers/url.js
+++ controllers/url.js
@@ -13,7 +13,10 @@ async function handleGenerateNewShortURL(req, res){
         visitHistory: [],
     });
 
-    return res.json({id: shortId});
+    return res.render("home", {
+        id: shortId,
+    })
+    // return res.json({id: shortId});
 }
 
 async function handleGetAnalytics(req, res){
--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const app = express();
 const PORT = 8001;
+const path = require('path');
 
 const {connectToMongoDB} = require("./connect");
 const urlRoute = require("./routes/url");
 
 const URL = require("./models/url");
+const { appendFileSync } = require('fs');
+
+const staticRoute = require("./routes/staticRouter");
 
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
 .then(()=> console.log("MongoDB connected"));
 
+app.use("/", staticRoute);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/url", urlRoute);
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
 
 app.get('/:shortId', async (req, res) =>{
     const shortId = req.params.shortId;
@@ -26,6 +37,14 @@ app.get('/:shortId', async (req, res) =>{
         },
     });
     res.redirect(entry.redirectUrl);
+});
+
+app.get("/test", async(req, res) =>{
+    const allUrls = await URL.find({});
+
+    return res.render("home", {
+        urls: allUrls,
+    })
 })
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
